f60a058f6cf53fa8a105fb757b61b4e312f94645
Ifeoluwa Arowosegbe <iifeooluwa.ao@gmail.com>
2017-07-30 20:04:59 +0100

    ser require abraham/twitteroauth
    Wrote Header Validation method following Twitter requirements
    Pulled in Abraham Twitter Library to make Twitter API requests.
    Made twitter secret into a constant

--- app/Http/Controllers/TwitterWebhookController.php
+++ app/Http/Controllers/TwitterWebhookController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class TwitterWebhookController extends Controller
 {
+    
+    const TWITTER_SECRET = env("TWITTER_API_SECRET");
+
     /**
      * Handles requests to the Twitter Webhook
      * 
@@ -17,14 +20,49 @@ class TwitterWebhookController extends Controller
         if ($request->isMethod('get') && $request->has('crc_token')) {
             
             $crc = $request->input('crc_token');
-            $secret = env("TWITTER_API_SECRET");
-            $hashDigest = base64_encode(hash_hmac('sha256', $crc, $secret, true));
+            $hashDigest = base64_encode(hash_hmac('sha256', $crc, TWITTER_SECRET, true));
             $response = ['response_token' => 'sha256=' . $hashDigest];
 
             return json_encode($response);
         }
 
-        $message = ['status' => 'error', 'message' => 'Missing required params'];
+        $message = ['status' => 'error', 'message' => 'Missing required params or method not allowed'];
         return json_encode($message);
+    }
+
+    /**
+     * Handles DM events
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function handleDMEvents(Request $request)
+    {
+        if ($this->validateHeader($request)) {
+            # code...
+        }
+    }
+
+    /**
+     * Validate request header. This is to ensure that the request originated from a trusted source.
+     * According to the Twitter API Validation rules.
+     * @param  [type] $request
+     * @return [bool] 
+     */
+    public function validateHeader($request)
+    {
+        $signature = $request->header('x-twitter-webhooks-signature');
+        $hashAlgo = explode('=', $signature)[0];
+
+        if ($request->secure() && $hashAlgo === 'sha256') {
+            
+            $payload = $request->getContent();
+            $payloadHashDigest = hash_hmac('sha256', $payload, TWITTER_SECRET);
+
+            if (hash_equals($payloadHashDigest, base64_encode($signature))) {
+                return true;
+            }
+        }else{
+            return false;
+        }
     }
 }
